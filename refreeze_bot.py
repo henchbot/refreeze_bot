c9894f521a93d0baa620cb883cf956822a40eeaa
henchc <chris.l.hench@gmail.com>
2019-08-25 17:42:17 -0400

    update for refreeze specifically

--- refreeze_bot.py
+++ refreeze_bot.py
@@ -6,14 +6,14 @@ import shutil
 import time
 import datetime
 
-REPO_API = 'https://api.github.com/repos/henchbot/refreeze_bot'
+REPO_API = 'https://api.github.com/repos/{}'
 NOTIFICATIONS_API = 'https://api.github.com/notifications'
 TOKEN = os.environ.get('HENCHBOT_TOKEN')
 
 
 class henchBotRefreeze:
     '''
-    Class for a bot that listens for a refreeze request repo2docker
+    Class for a bot that listens for a refreeze request in repo2docker
     '''
     def __init__(self):
         pass
@@ -24,6 +24,7 @@ class henchBotRefreeze:
         if res:
             for mention in res:
                 is_pr = bool(mention['subject']['type'] == 'PullRequest')
+                self.origin_repo = mention['repository']['full_name']
                 if mention['subject']['latest_comment_url']:
                     if is_pr and self.asked_for_refreeze(mention['subject']['latest_comment_url']):
                         self.refreeze_deps(mention)
@@ -42,20 +43,17 @@ class henchBotRefreeze:
     def refreeze_deps(self, mention):
         pr_info = self.get_pr_info(mention)
         self.clone_and_checkout_branch(pr_info)
-
-        os.chdir(pr_info['head']['repo']['name'])
         self.run_refreeze_commands()
         self.add_commit_push(pr_info)
         os.chdir('..')
         shutil.rmtree(pr_info['head']['repo']['name'])
-
         self.comment_on_pr(pr_info)
 
     def comment_on_pr(self, pr_info):
-    	body_text = ''' '''
+    	body_text = '''I changed some stuff you're welcome.'''
     	body = {'body': body_text}
-    	requests.post(REPO_API + '/issues/' + pr_info['number'] + '/comments',
-    		          headers={'Authorization': 'token {}'.format(TOKEN)}, json=body))
+    	requests.post(REPO_API.format(self.origin_repo) + '/issues/' + str(pr_info['number']) + '/comments',
+    		          headers={'Authorization': 'token {}'.format(TOKEN)}, json=body)
 
     def get_pr_info(self, mention):
         res = requests.get(mention['subject']['url']).json()
@@ -63,19 +61,19 @@ class henchBotRefreeze:
 
     def clone_and_checkout_branch(self, pr_info):
         subprocess.check_output(['git', 'clone', pr_info['head']['repo']['html_url']])
+        os.chdir(pr_info['head']['repo']['name'])
+        subprocess.check_output(['git', 'fetch', 'origin', pr_info['head']['ref']])
         subprocess.check_output(['git', 'checkout', pr_info['head']['ref']])
 
     def add_commit_push(self, pr_info):
-        subprocess.check_output(['git', 'add', 'README.md'])
+        subprocess.check_output(['git', 'add', '-A'])
         subprocess.check_output(['git', 'commit', '-m', 'made a change'])
         subprocess.check_output(['git', 'push', 'origin', pr_info['head']['ref']])
 
     def run_refreeze_commands(self):
-        with open('README.md', 'r') as f:
-            raw = f.read()
-        with open('README.md', 'w') as f:
-            f.write(raw + '\n test!')
-
+        os.chdir('repo2docker/buildpacks/conda')
+        subprocess.check_output(['python', 'freeze.py'])
+        os.chdir('../../..')
 
 
 if __name__ == '__main__':
